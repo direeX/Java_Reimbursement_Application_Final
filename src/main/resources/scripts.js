c6a9bce4bbdf7a47e6e62be8ba7bd577f504ba8b
direeX <dominik.obara7@gmail.com>
2023-08-11 23:11:53 +0200

    Starting point 2

---
+++ src/main/resources/scripts.js
@@ -0,0 +1,62 @@
+
+function goToUser() {
+    window.location.href = "/user.html";
+    console.log("asd")
+}
+
+function goToAdmin() {
+    window.location.href = "/admin.html";
+}
+
+function changeBackground() {
+    document.body.style.backgroundColor = "#34a853"; // green
+}
+
+function backToHome() {
+    window.location.href = "/";
+}
+
+function checkLogin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Dummy check for this example
+    if(username === "admin" && password === "admin") {
+        document.getElementById("loginForm").style.display = "none";
+        document.getElementById("adminBtn").style.display = "block";
+    }
+}
+
+let receipts = [];
+
+function addReceipt() {
+    const receiptType = document.getElementById("receiptType").value;
+    const receiptValue = document.getElementById("receiptValue").value;
+    receipts.push({ type: receiptType, value: parseFloat(receiptValue) });
+
+    const list = document.getElementById("receiptList");
+    const item = document.createElement("li");
+    item.textContent = `${receiptType}: ${receiptValue} zł`;
+    list.appendChild(item);
+}
+
+function addReceipt() {
+    const receiptType = document.getElementById('receiptType').value;
+    const receiptValue = document.getElementById('receiptValue').value;
+
+    if (!receiptValue) {
+        alert('Proszę podać wartość paragonu.');
+        return;
+    }
+
+    const receiptList = document.getElementById('receiptList');
+    const listItem = document.createElement('li');
+    listItem.textContent = `${receiptType}: ${receiptValue} zł`;
+    receiptList.appendChild(listItem);
+}
+
+function submitApplication() {
+    // Tutaj będziemy przetwarzać wniosek i wysyłać go do serwera.
+    // Na razie dodaję alert dla symulacji.
+    alert("Wniosek został wysłany!");
+}
