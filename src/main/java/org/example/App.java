8a347ed9ce2bf4b7f325da02f425000234afa090
direeX <dominik.obara7@gmail.com>
2023-08-11 21:22:05 +0200

    Starting point

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,2 +1,23 @@
-package org.example;public class App {
+package org.example;
+
+
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.createContext("/", new MyHandler());
+            server.setExecutor(null); // tworzy domyślny executor
+            server.start();
+            System.out.println("Server started on port 8080");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/MyHandler.java
+++ src/main/java/org/example/MyHandler.java
@@ -1,2 +1,41 @@
-package org.example;public class MyHandler {
+package org.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MyHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        byte[] response;
+        if ("/".equals(path)) {
+            response = loadResource("index.html");
+            exchange.sendResponseHeaders(200, response.length);
+        } else {
+            try {
+                response = loadResource(path.substring(1));
+                exchange.sendResponseHeaders(200, response.length);
+            } catch (FileNotFoundException e) {
+                response = "404 Not Found".getBytes();
+                exchange.sendResponseHeaders(404, response.length);
+            }
+        }
+        OutputStream os = exchange.getResponseBody();
+        os.write(response);
+        os.close();
+    }
+
+    private byte[] loadResource(String resourceName) throws IOException {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(resourceName);
+        if (is == null) {
+            throw new FileNotFoundException("Plik " + resourceName + " nie został znaleziony.");
+        }
+        return is.readAllBytes();
+    }
 }
